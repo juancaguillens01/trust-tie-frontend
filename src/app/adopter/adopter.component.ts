49b66078ad291d7588af1da7d582152ec9af46dd
juancaguillens01 <jguillenw@alumnos.unex.es>
2024-06-05 19:53:04 +0200

    Fixed problem of active links in header and changed CSS files to SCSS files

--- src/app/adopter/adopter.component.ts
+++ src/app/adopter/adopter.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-adopter',
   templateUrl: './adopter.component.html',
-  styleUrls: ['./adopter.component.css']
+  styleUrls: ['./adopter.component.scss']
 })
 export class AdopterComponent {
 
--- src/app/organization/organization.component.ts
+++ src/app/organization/organization.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-organization',
   templateUrl: './organization.component.html',
-  styleUrls: ['./organization.component.css']
+  styleUrls: ['./organization.component.scss']
 })
 export class OrganizationComponent {
 

c66c330b248b2c6a0e17d31155da7d7626a279da
juancaguillens01 <jguillenw@alumnos.unex.es>
2024-06-13 20:47:57 +0200

    Created guard for avoiding unnecessary logins and registers

--- src/app/adopter/adopter-routing.module.ts
+++ src/app/adopter/adopter-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdopterComponent } from './adopter.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import {LoggedInGuard} from "@core/guards/logged-in-guard";
 
 const routes: Routes = [
   { path: '', component: AdopterComponent, children: [
-      { path: 'login', component: LoginComponent },
-      { path: 'register', component: RegisterComponent }
+      { path: 'login', component: LoginComponent, canActivate: [LoggedInGuard] },
+      { path: 'register', component: RegisterComponent, canActivate: [LoggedInGuard] }
     ]}
 ];
 
---
+++ src/app/core/guards/logged-in-guard.ts
@@ -0,0 +1,18 @@
+import {CanActivateFn, Router} from '@angular/router';
+import {inject} from '@angular/core';
+import {AuthService} from '@core/auth.service';
+
+export const LoggedInGuard: CanActivateFn = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  if (!authService.checkIsLoggedIn()) {
+    return true;
+  } else if (authService.checkIsOrganization()) {
+    router.navigate(['/organization/dashboard']).then();
+    return false;
+  } else if (authService.checkIsAdopter()) {
+    router.navigate(['/adopter']).then();
+    return false;
+  }
+  return false;
+};

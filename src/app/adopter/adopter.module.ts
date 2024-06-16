159e5f985f1c41966dfad4386a7df08bf8fae7d7
juancaguillens01 <jguillenw@alumnos.unex.es>
2024-06-16 22:47:47 +0200

    Added necessary modules in adopter

--- src/app/adopter/adopter.module.ts
+++ src/app/adopter/adopter.module.ts
@@ -1,21 +1,22 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
-import { AdopterRoutingModule } from './adopter-routing.module';
-import { AdopterComponent } from './adopter.component';
+import {AdopterRoutingModule} from './adopter-routing.module';
+import {AdopterComponent} from './adopter.component';
 import {MatButtonModule} from "@angular/material/button";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatCardModule} from "@angular/material/card";
 import {MatInputModule} from "@angular/material/input";
-import { LoginComponent } from './login/login.component';
+import {LoginComponent} from './login/login.component';
 import {ReactiveFormsModule} from "@angular/forms";
-import { RegisterComponent } from './register/register.component';
-import {MatOptionModule} from "@angular/material/core";
+import {RegisterComponent} from './register/register.component';
+import {MatNativeDateModule, MatOptionModule} from "@angular/material/core";
 import {MatSelectModule} from "@angular/material/select";
 import {SharedModule} from "../shared/shared.module";
-import { AdopterProfileComponent } from './profile/adopter-profile.component';
-import { AnimalsListComponent } from './animals/animals-list.component';
+import {AdopterProfileComponent} from './profile/adopter-profile.component';
+import {AnimalsListComponent} from './animals/animals-list.component';
 import {MatListModule} from "@angular/material/list";
-import { EventsListComponent } from './events/events-list.component';
+import {EventsListComponent} from './events/events-list.component';
+import {MatDatepickerModule} from "@angular/material/datepicker";
 
 
 @NgModule({
@@ -27,19 +28,22 @@ import { EventsListComponent } from './events/events-list.component';
     AnimalsListComponent,
     EventsListComponent
   ],
-    imports: [
-        CommonModule,
-        AdopterRoutingModule,
-        MatButtonModule,
-        MatToolbarModule,
-        NgOptimizedImage,
-        MatCardModule,
-        MatInputModule,
-        ReactiveFormsModule,
-        MatOptionModule,
-        MatSelectModule,
-        SharedModule,
-        MatListModule
-    ]
+  imports: [
+    CommonModule,
+    AdopterRoutingModule,
+    MatButtonModule,
+    MatToolbarModule,
+    NgOptimizedImage,
+    MatCardModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatOptionModule,
+    MatSelectModule,
+    SharedModule,
+    MatListModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ]
 })
-export class AdopterModule { }
+export class AdopterModule {
+}
